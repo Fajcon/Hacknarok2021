12fb60120d53f9d926d8dffaa4051c5fa27a8098
Fajcon <kuba.ficon23@gmail.com>
2021-03-28 06:55:57 +0200

    nice x2

--- Assets/Scripts/ProductsController.cs
+++ Assets/Scripts/ProductsController.cs
@@ -1,26 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class ProductsController : MonoBehaviour
 {
-    public GameObject rightHand;
-    
-    void OnCollisionEnter (Collision col)
-    {
-        Debug.Log(col.gameObject.tag);
-        if (col.gameObject.tag.Equals("hand"))
-        {
-            Debug.Log("dupa");
-        }
 
-    }
+    public String name;
+    public String price;
     
+    public void ProductDetails()
+    {
+        
+    }
+
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     Debug.Log(collision.gameObject.tag);
+    //     if (collision.gameObject.tag == "shopcart")
+    //     {
+    //         Debug.Log(collision.gameObject.tag);
+    //         Debug.Log(name);
+    //     }
+    // }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
